ad7fcb138f071b88bcbad40b11a45c105dc2fe6e
Dhaaraniguru <dhaaraniguru5@gmail.com>
2025-05-15 13:22:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 with st.sidebar:
     select=option_menu(
-        menu_title="Intenship",
+        menu_title="Vinsup Infotech",
         options=["Intenship","About","contact"],
         icons=['book','file-person','telephone']
     )
